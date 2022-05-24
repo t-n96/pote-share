a70361590c09e3c2eed996b4c6be9056b9440c82
nozomi tanaka <nozomitanaka@nozomi-no-MacBook-Air.local>
2022-05-24 23:26:56 +0900

    ログイン実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) 
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
+  end
+
+  def after_sign_in_path_for(resource)
+    if current_user
+      flash[:notice] = "ログインしました"
+      root_path
+    else
+      flash[:notice] = "新規登録完了しました"
+      edit_user_registration_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+      root_path
+  end
+
 end
